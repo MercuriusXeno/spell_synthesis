68a8ed8e43a450da6fac38cccae4e953cc00e211
Lamia <xaxnas@gmail.com>
2024-11-18 13:22:35 +0000

    digger evo

--- files/spells/evolutions/spells.lua
+++ files/spells/evolutions/spells.lua
@@ -38,6 +38,39 @@ local spells = {
 			c.damage_critical_chance = c.damage_critical_chance + 10
 		end,
 	},
+	{
+		id = "SS_DIGGER_2",
+		name = "$action_digger",
+		description = "$actiondesc_digger",
+		sprite = "mods/spell_synthesis/files/spells/evolutions/digger/digger_2.png",
+		related_projectiles = { "mods/spell_synthesis/files/spells/evolutions/digger/ss_digger_2.xml" },
+		type = ACTION_TYPE_PROJECTILE,
+		spawn_level = "10",
+		spawn_probability = "0",
+		mana = 1,
+		sound_loop_tag = "sound_digger",
+		action = function()
+			add_projectile("mods/spell_synthesis/files/spells/evolutions/digger/ss_digger_2.xml")
+			c.fire_rate_wait = c.fire_rate_wait + 0.5
+			current_reload_time = current_reload_time - ACTION_DRAW_RELOAD_TIME_INCREASE - 12
+		end,
+	},
+	{
+		id = "SS_DIGGER_3",
+		name = "$action_digger",
+		description = "$actiondesc_digger",
+		sprite = "mods/spell_synthesis/files/spells/evolutions/digger/digger_3.png",
+		related_projectiles = { "mods/spell_synthesis/files/spells/evolutions/digger/ss_digger_3.xml" },
+		type = ACTION_TYPE_PROJECTILE,
+		spawn_level = "10",
+		spawn_probability = "0",
+		mana = 2,
+		sound_loop_tag = "sound_digger",
+		action = function()
+			add_projectile("mods/spell_synthesis/files/spells/evolutions/digger/ss_digger_3.xml")
+			current_reload_time = current_reload_time - ACTION_DRAW_RELOAD_TIME_INCREASE - 15
+		end,
+	},
 }
 
 if actions then
--- files/spells/recipes/recipes.lua
+++ files/spells/recipes/recipes.lua
@@ -8,4 +8,20 @@ SSRecipes = {
 		spells = { "SS_LIGHT_BULLET_2", "SS_LIGHT_BULLET_2" },
 		result = "SS_LIGHT_BULLET_3",
 	},
+	{
+		spells = { "DIGGER", "DIGGER" },
+		result = "SS_DIGGER_2",
+	},
+	{
+		spells = { "DIGGER", "POWERDIGGER" },
+		result = "SS_DIGGER_2",
+	},
+	{
+		spells = { "POWERDIGGER", "POWERDIGGER" },
+		result = "SS_DIGGER_2",
+	},
+	{
+		spells = { "SS_DIGGER_2", "SS_DIGGER_2" },
+		result = "SS_DIGGER_3",
+	},
 }
