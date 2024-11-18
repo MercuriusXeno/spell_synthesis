46a3953f49259a072f909201ea42b8acd45ba5d9
Lamia <xaxnas@gmail.com>
2024-11-18 11:08:24 +0000

    added workshop area to fuser

--- files/entities/fuser/base/fuser_create.lua
+++ files/entities/fuser/base/fuser_create.lua
@@ -7,12 +7,17 @@ if not DoesWorldExistAt(pos_x, pos_y - search_dist_y, pos_x, pos_y) then return 
 
 local ray_hit, ray_x, ray_y = RaytracePlatforms(pos_x, pos_y, pos_x, pos_y + search_dist_y)
 if ray_hit then
-	EntitySetTransform(entity, ray_x, ray_y - 23)
+	local entity_y = ray_y - 23
+	EntitySetTransform(entity, ray_x, entity_y)
+	if true then
+		local workshop_entity = EntityLoad("mods/spell_synthesis/files/entities/fuser/base/fuser_workshop.xml", ray_x, entity_y)
+		EntityAddChild(entity, workshop_entity)
+	end
 	LoadPixelScene(
 		"mods/spell_synthesis/files/entities/fuser/pixelscene/fuser.png",
 		"mods/spell_synthesis/files/entities/fuser/pixelscene/fuser_visual.png",
 		ray_x - 35,
-		ray_y - 42
+		entity_y - 19
 	)
 	EntityRemoveComponent(entity, GetUpdatedComponentID())
 else
