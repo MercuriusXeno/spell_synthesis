d30c61e929d19a198ea731a2faef7bf09c6a74c6
Lamia <xaxnas@gmail.com>
2024-11-22 21:18:06 +0000

    no need

--- init.lua
+++ init.lua
@@ -4,10 +4,3 @@ dofile_once("mods/spell_synthesis/files/spells/append.lua")
 
 ModRegisterAudioEventMappings("mods/spell_synthesis/files/audio/GUIDs.txt")
 ModRegisterMusicBank("mods/spell_synthesis/files/audio/bank/spell_synthesis.bank")
-
---- Blahblah
-function OnPlayerSpawned()
-	if GameHasFlagRun("spell_synthesis_init") then return end
-	GameAddFlagRun("spell_synthesis_init")
-	-- EntityLoad("mods/spell_synthesis/files/entities/fuser/fuser_world.xml", 180, -100)
-end
