bb6782a83ae0fbbb0bd49539ae452907bb263197
Lamia <xaxnas@gmail.com>
2024-11-22 21:18:09 +0000

    copis

--- files/spells/append.lua
+++ files/spells/append.lua
@@ -8,3 +8,11 @@ for i = 1, #spell_list do
 	dofile_once(spells_file) -- going once manually to populate icons
 	ModLuaFileAppend("data/scripts/gun/gun_actions.lua", spells_file)
 end
+
+local compat_list = {
+	copis_things = "mods/spell_synthesis/files/spells/compatibility/copis_things/spells.lua",
+}
+
+for mod, append_file in pairs(compat_list) do
+	if ModIsEnabled(mod) then ModLuaFileAppend("data/scripts/gun/gun_actions.lua", append_file) end
+end
