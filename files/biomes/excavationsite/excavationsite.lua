2e354ab85cc6a5f5f149a89a4b4ebba53738d39b
Lamia <xaxnas@gmail.com>
2024-11-23 13:48:22 +0000

    fuser on level 2

--- files/biomes/excavationsite/excavationsite.lua
+++ files/biomes/excavationsite/excavationsite.lua
@@ -1,9 +1,14 @@
 ---@diagnostic disable: lowercase-global, missing-global-doc
+RegisterSpawnFunction(0xff0abae0, "ss_spawn_fuser")
 
--- g_pixel_scene_04[#g_pixel_scene_04 + 1] = {
--- 	prob = 1000,
--- 	material_file = "mods/spell_synthesis/files/biomes/excavationsite/saw_puzzle/saw_puzzle.png",
--- 	visual_file = "mods/spell_synthesis/files/biomes/excavationsite/saw_puzzle/saw_puzzle_visual.png",
--- 	background_file = "",
--- 	is_unique = 0,
--- }
+function ss_spawn_fuser(x, y)
+	EntityLoad("mods/spell_synthesis/files/entities/fuser/fuser_no_base.xml", x, y)
+end
+
+g_pixel_scene_04[#g_pixel_scene_04 + 1] = {
+	prob = 0.3,
+	material_file = "mods/spell_synthesis/files/biomes/excavationsite/fuser.png",
+	visual_file = "mods/spell_synthesis/files/biomes/excavationsite/fuser_visual.png",
+	background_file = "mods/spell_synthesis/files/biomes/excavationsite/fuser_background.png",
+	is_unique = 0,
+}
