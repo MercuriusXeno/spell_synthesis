99f9bb9579c6a565e600800626c56733894cba92
Lamia <xaxnas@gmail.com>
2024-11-23 13:09:44 +0000

    whoops

--- files/biomes/snowcave/snowcave.lua
+++ files/biomes/snowcave/snowcave.lua
@@ -6,7 +6,7 @@ function ss_spawn_saw_puzzle(x, y)
 end
 
 g_pixel_scene_02[#g_pixel_scene_02 + 1] = {
-	prob = 1000,
+	prob = 0.2,
 	material_file = "mods/spell_synthesis/files/biomes/snowcave/saw_puzzle/saw_puzzle.png",
 	visual_file = "mods/spell_synthesis/files/biomes/snowcave/saw_puzzle/saw_puzzle_visual.png",
 	background_file = "",
